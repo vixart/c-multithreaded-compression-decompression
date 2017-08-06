b1f222835f84cfbab0dc2fecc818052f7d5817e2
vixart <vixmontreal@gmail.com>
2017-08-06 19:49:54 +0300

    commit

--- Program.cs
+++ Program.cs
@@ -80,6 +80,8 @@ namespace MtCompressing
                     {
                         if (threads[cycleCount].ThreadState == ThreadState.Stopped)
                         {
+                            BitConverter.GetBytes(compressedDataArray[cycleCount].Length)
+                                           .CopyTo(compressedDataArray[cycleCount], 4);
                             outFile.Write(compressedDataArray[cycleCount], 0, compressedDataArray[cycleCount].Length);
                             cycleCount++;
                         }
@@ -111,75 +113,45 @@ namespace MtCompressing
 
         public void Decompress(string inName, string outName)
         {
+            if (!File.Exists(inName))
+            {
+                Console.WriteLine("File does not exist");
+                return;
+            }
+                
+            if (File.Exists(outName))
+                File.Delete(outName);
+
+            FileStream inFile = new FileStream(inName, FileMode.Open);
+            FileStream outFile = new FileStream(outName, FileMode.Append);
+            Thread[] threads;
+
+            dataArray = new byte[threadsCount][];
+            compressedDataArray = new byte[threadsCount][];
+            byte[] buffer = new byte[8];
+            int compressedBlockLength = 0;
+            uint currentDataSize;
+
+            Console.WriteLine("Start Decomressing");
             try
             {
-                if (!File.Exists(inName))
-                {
-                    Console.WriteLine("File does not exist");
-                    return;
-                }
-                
-                if (File.Exists(outName))
-                    File.Delete(outName);
-
-                FileStream inFile = new FileStream(inName, FileMode.Open);
-                FileStream outFile = new FileStream(outName, FileMode.Append);
-                Thread[] threads;
-
-                dataArray = new byte[threadsCount][];
-                compressedDataArray = new byte[threadsCount][];
-                byte[] idBuffer = new byte[3];
-                int currentPosition = 0;
-                int lastBlockEndPosition = 0;
-                int compressedBlockLength = 0;
-                int currentDataSize;
-
-                Console.WriteLine("Start Decomressing");
                 while (inFile.Position < inFile.Length)
                 {
                     threads = new Thread[threadsCount];
                     for (int cycleCount = 0; (cycleCount < threadsCount) && (inFile.Position < inFile.Length);
-                         cycleCount++)
+                            cycleCount++)
                     {
-                        while (inFile.Position < inFile.Length)
-                        {
-                            // Использую ID1, ID2 и CM(байты хэдера gzip) для разделения сжатых блоков
-                            idBuffer[2] = idBuffer[1];
-                            idBuffer[1] = idBuffer[0];
-                            inFile.Read(idBuffer, 0, 1);
-                            currentPosition++;
+                        inFile.Read(buffer, 0, 8);
+                        compressedBlockLength = BitConverter.ToInt32(buffer, 4);
+                        compressedDataArray[cycleCount] = new byte[compressedBlockLength];
+                        buffer.CopyTo(compressedDataArray[cycleCount], 0);
 
-                            // если ID1 = 31, ID2 = 139 и CM = 8(режим сжатия: deflate) - мы нашли новый блок
-                            // Первый блок нам и так известен, поэтому пропустим его(currentPosition > 3)
-                            if ((int)idBuffer[0] == 8 && (int)idBuffer[1] == 139 && (int)idBuffer[2] == 31 && currentPosition > 3)
-                            {
-                                // Вычисляю длину сжатого блока
-                                compressedBlockLength = currentPosition - lastBlockEndPosition - 3;
-                                compressedDataArray[cycleCount] = new byte[compressedBlockLength];
-
-                                // Сдвигаюсь на конец последнего разжатого блока в потоке
-                                inFile.Seek(lastBlockEndPosition, SeekOrigin.Begin);
-
-                                // Читаю сжатый блок в массив
-                                inFile.Read(compressedDataArray[cycleCount], 0, compressedBlockLength);
-                                
-                                // Использую последние 4 байта сжатого блока(ISIZE в спецификации gzip), чтобы узнать размер разжатных данных 
-                                currentDataSize = BitConverter.ToInt32(compressedDataArray[cycleCount], compressedBlockLength - 4);
-                                dataArray[cycleCount] = new byte[currentDataSize];
-
-                                // Сохраняю конец последнего разжатого блока
-                                lastBlockEndPosition = currentPosition - 3;
-
-                                // Сдвигаюсь на текущую позицию в потоке
-                                inFile.Seek(currentPosition, SeekOrigin.Begin);
-
-                                // Запускаю поток рзжатия блока данных
-                                threads[cycleCount] = new Thread(DecompressBlock);
-                                threads[cycleCount].Start(cycleCount);
-
-                                break;
-                            }
-                        }
+                        inFile.Read(compressedDataArray[cycleCount], 8, compressedBlockLength - 8);
+                        currentDataSize = BitConverter.ToUInt32(compressedDataArray[cycleCount], compressedBlockLength - 4);
+                        dataArray[cycleCount] = new byte[currentDataSize];
+                        
+                        threads[cycleCount] = new Thread(DecompressBlock);
+                        threads[cycleCount].Start(cycleCount);
                     }
 
                     for (int cycleCount = 0; (cycleCount < threadsCount) && (threads[cycleCount] != null);)
@@ -191,15 +163,15 @@ namespace MtCompressing
                         }
                     }
                 }
-
-                outFile.Close();
-                inFile.Close();
-                Console.WriteLine("End Decompressing");
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
                 Console.WriteLine("ERROR:" + ex.Message);
             }
+
+            outFile.Close();
+            inFile.Close();
+            Console.WriteLine("End Decompressing");
         }
 
         private void DecompressBlock(object cycleCount)
